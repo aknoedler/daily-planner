6105a56c59e406b5d1e3e06558f238d27b1b946b
aknoedler <andrewknoedler89@gmail.com>
2022-10-29 10:46:52 -0500

    added storage functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,35 @@
 var currentTime = moment();
 var containerEl = $(".container");
+var storedTasks = JSON.parse(localStorage.getItem('storedTasks'));
 
 function init() {
     $('#currentDay').text(currentTime.format("dddd, MMMM Do"));
 
+    if (storedTasks == null) {
+        storedTasks = [];
+        for (i = 0; i < 9; i++) {
+            storedTasks.push('');
+        }
+    }
+
     for (i = 9; i <= 17; i++) {
-        var row = $('<div>');
-        row.addClass('row time-block');
-        
+        var row = $('<div class = "row time-block"></div');
+
         var hourIndex = i;
 
-        if (hourIndex < currentTime.format('H')){
+        if (hourIndex < currentTime.format('H')) {
             row.addClass('past');
-        } else if (hourIndex == currentTime.format('H')){
+        } else if (hourIndex == currentTime.format('H')) {
             row.addClass('present');
         } else {
             row.addClass('future');
         }
 
         var period = "am";
-        if (hourIndex>=12){
+        if (hourIndex >= 12) {
             period = "pm";
         }
-        if (hourIndex>=13){
+        if (hourIndex >= 13) {
             hourIndex -= 12;
         }
 
@@ -32,14 +39,23 @@ function init() {
         row.append(hour);
 
         var textArea = $('<textarea class="description col-md-10"></textarea');
+        textArea.text(storedTasks[i - 9]);
         row.append(textArea);
 
         var btn = $('<button class="saveBtn col-md-1"></button>');
         btn.append($('<i class ="fas fa-save"></i>'));
         row.append(btn);
-        
+
         containerEl.append(row);
     }
+
 }
 
 init();
+
+$('.saveBtn').on('click', function () {
+    var task = $(this).siblings('.description').val();
+    var index = $(this).parent().index();
+    storedTasks[index] = task
+    localStorage.setItem('storedTasks', JSON.stringify(storedTasks));
+    })
