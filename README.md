32c21add285445c2159313f1bc7bf229e30b8765
aknoedler <andrewknoedler89@gmail.com>
2022-10-31 21:49:02 -0500

    added comments and README

---
+++ README.md
@@ -0,0 +1,25 @@
+# Daily Planner
+
+This project uses jQuery, Bootstrap, and Moment.js to create a daily planner. The planner is split into hourly categories. Enteries are saved into local storage by pressing the save button to the right of the entry. The site uses Moment.js to evaluate the local time and color codes the enteries based on whether they have passed, are currently happening, or are in the future.
+
+## Installation
+
+N/A
+
+## Usage
+
+[Live website](https://aknoedler.github.io/daily-planner/)
+
+Enter tasks into the hourly sections, then click the save button to save to local storage.
+
+## Screenshots
+
+![Daily Planner](./assets/screenshots/screenshot.png)
+
+## Contributing
+
+N/A
+
+## License
+
+N/A
--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,11 @@ var containerEl = $(".container");
 var storedTasks = JSON.parse(localStorage.getItem('storedTasks'));
 
 function init() {
+    // Sets the current day header to display the current day
     $('#currentDay').text(currentTime.format("dddd, MMMM Do"));
 
+    // The storedTasks array is pulled from local storage on page load; if it is empty,
+    // this conditional initializes it as an array of empty strings
     if (storedTasks == null) {
         storedTasks = [];
         for (i = 0; i < 9; i++) {
@@ -12,11 +15,13 @@ function init() {
         }
     }
 
+    // Creates the hourly blocks, iterating from 9 to 17
     for (i = 9; i <= 17; i++) {
         var row = $('<div class = "row time-block"></div');
 
         var hourIndex = i;
 
+        // Formats the blocks by comparing to current time
         if (hourIndex < currentTime.format('H')) {
             row.addClass('past');
         } else if (hourIndex == currentTime.format('H')) {
@@ -25,20 +30,25 @@ function init() {
             row.addClass('future');
         }
 
+        // Sets the text to am or pm
         var period = "am";
         if (hourIndex >= 12) {
             period = "pm";
         }
+
+        // Convert the text to 12 hour time
         if (hourIndex >= 13) {
             hourIndex -= 12;
         }
 
+        // Create the hour, textarea, and button elements and appends them to the row element
         var hour = $('<div class = "hour col-md-1"></div>');
         hour.text(hourIndex + period);
 
         row.append(hour);
 
         var textArea = $('<textarea class="description col-md-10"></textarea');
+        // Populates with saved tasks if they were loaded
         textArea.text(storedTasks[i - 9]);
         row.append(textArea);
 
@@ -46,6 +56,7 @@ function init() {
         btn.append($('<i class ="fas fa-save"></i>'));
         row.append(btn);
 
+        // Append the row to the main container
         containerEl.append(row);
     }
 
@@ -53,6 +64,7 @@ function init() {
 
 init();
 
+// Save the text in the textarea elements to local storage
 $('.saveBtn').on('click', function () {
     var task = $(this).siblings('.description').val();
     var index = $(this).parent().index();
